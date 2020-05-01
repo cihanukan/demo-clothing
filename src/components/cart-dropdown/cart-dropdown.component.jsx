2db372d573a73a19260c6b0d432bc1c1a175d2ed
cihan_ukan <cihan.ukan@gmail.com>
2020-05-01 18:58:03 +0300

    updating application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.compnent";
 import "./card-dropdown.styles.scss";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = ({cartItems}) => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {
-          cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-        }
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
       </div>
-      <CustomButton >GO TO CHECKOUT</CustomButton>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-const mapStateToProps = ({cart: {cartItems}}) =>({
-  cartItems
-})
+const mapStateToProps = (state) => ({
+  //Adding memoization to state call
+  cartItems: selectCartItems(state),
+});
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart; // input selector. It is a function to get whole state and just returns a slice of it.
+
+//output selector. It is going to use input selector as an argument.
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+//get the cartItems and calculate quantity. We have memoized count and cartItems array.
+//There is no need to calculate every irrelevant state call such as currentUser.
+//Otherwise every call on mapStateToProps would cause to recalculate quantity even there wasn't any changes on it.
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce((accumulator, item) => accumulator + item.quantity, 0)
+);
