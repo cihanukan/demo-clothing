292b13f48594166454f8ed9165d29728b751f6b1
cihan_ukan <cihan.ukan@gmail.com>
2020-05-12 16:43:02 +0300

    Added spinner affect to the firease data flow

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -12,11 +12,12 @@ export const selectShopCollections = createSelector(
 //Object.keys ==> convert object to array. We should convert the object for mapping on it
 export const selectCollectionsForPreview = createSelector(
   [selectShopCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [selectShopCollections],
-    (collections) => collections[collectionUrlParam]
+    (collections) => collections ? collections[collectionUrlParam] : null
   );
