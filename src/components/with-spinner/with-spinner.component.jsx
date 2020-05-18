53f0ee21a72dd2cee52c55c5ca36b4c4db665312
cihan_ukan <cihan.ukan@gmail.com>
2020-05-19 02:54:53 +0300

    Quick arrangement to the WithSpinner HOC.

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -3,17 +3,15 @@ import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
 //Defining HOC
-
-const WithSpinner = (WrappedComponent) => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ? (
-      <SpinnerOverlay>
-        <SpinnerContainer />
-      </SpinnerOverlay>
-    ) : (
-      <WrappedComponent {...otherProps} />
-    );
-  };
-  return Spinner;
+//It will get a component as a props and will pass to the another component to check loading status
+const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  ) : (
+    <WrappedComponent {...otherProps} />
+  );
 };
+
 export default WithSpinner;
